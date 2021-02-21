cd5694c5dab329cc291b7f04aa7a27894c3b5139
Manish Ahire <ahiremanishb@gmail.com>
2021-02-21 14:47:58 +0530

    Added marks

--- StoreDataInKeychain/ViewController.swift
+++ StoreDataInKeychain/ViewController.swift
@@ -9,13 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //MARK:- Outlets
     @IBOutlet weak var txtPassword: UITextField!
+    
+    //MARK:- View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    //MARK:- Button Actions
     @IBAction func actionOnSavePassword(_ sender: Any) {
     }
 }
